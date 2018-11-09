b68067e72242f1512bb1512238b766e0d7df4aa2
Paula Lopez <paula.lopezpozuelo@intercom.io>
2018-11-09 08:41:43 +0000

    Chapter 9 - ember data

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,42 +1,7 @@
 import Route from '@ember/routing/route';
-import { A } from '@ember/array';
-import Band from 'ember-rock/models/band'
-import Song from 'ember-rock/models/song'
 
 export default Route.extend({
   model() {
-    let blackDog = Song.create({
-      title: 'Black Dog',
-      band: 'Led Zeppelin',
-      rating: 3,
-    });
-
-    let yellowLedbetter = Song.create({
-      title: 'Yellow Ledbetter',
-      band: 'Pearl Jam',
-      rating: 4,
-    });
-
-    let pretender = Song.create({
-      title: 'The Pretender',
-      band: 'Foo Fighters',
-      rating: 2,
-    });
-
-    let daughter = Song.create({
-      title: 'Daughter',
-      band: 'Pearl Jam',
-      rating: 5,
-    });
-
-    let ledZeppelin = Band.create({ name: 'Led Zeppelin', songs: A([blackDog]) });
-    let pearlJam = Band.create({
-      name: 'Pearl Jam',
-      description: 'Pearl Jam is an American rock band, formed in Seattle, Washington in 1990.',
-      songs: A([yellowLedbetter, daughter])
-    });
-    let fooFighters = Band.create({ name: 'Foo fighters', songs: A([pretender]) });
-
-    return A([ledZeppelin, pearlJam, fooFighters]);
+    return this.store.findAll('band');
   }
 });
